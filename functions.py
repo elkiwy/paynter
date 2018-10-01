ac9fab51712d739baa936e3369f9778a21389efa
Stefano Bertoli <elkiwydev@gmail.com>
2018-10-01 20:25:23 +0200

    Added angled and multiple-tips brushes, still supporting black-alpha brushtip images and not BW images

--- functions.py
+++ functions.py
@@ -1,5 +1,27 @@
 import numpy as N
 from PIL import Image
+import PIL.ImageOps
+import random
+
+def rand(a,b):
+	return random.randint(a,b) #inclusive
+
+
+
+'''
+TODO:
+-Support BW brush tips and not alpha tip
+-Do a proper wrapping around brush textures
+
+
+'''
+
+
+
+
+
+
+
 
 class Paynter:
 	brush = 0
@@ -7,7 +29,7 @@ class Paynter:
 	color = [0,0,0,0]
 
 	def drawLine(self, x1, y1, x2, y2):
-		for i in N.arange(0, 1, 0.01):
+		for i in N.arange(0, 1, 1):
 			x = x1 + i*(x2-x1)
 			y = y1 + i*(y2-y1)
 			#self.layer[int(x), int(y)] = self.color
@@ -30,79 +52,78 @@ class Brush:
 	#fuzzySize = [0.45, 0.55]
 	brushSize = 0
 	maskSize = 0
+	multibrush = False
 
-	def __init__(self, tipImage, maskImage, size=50, color=[0,0,0,0]):
+	def __init__(self, tipImage, maskImage, size=50, color=[0,0,0,0],angle=0):
 		#Set the brushTip
-		res = Image.open("gradient_white.png")
-		factor = (size/res.width)
-		resScaled = res.resize((int(res.width * factor), int(res.height * factor)))
-		alpha = resScaled.split()[-1]
+		if isinstance(tipImage, list):
+			#Multibrush
+			self.brushTip = []
+			for image in tipImage:
+				res = Image.open(image)
+				res = res.rotate(angle, expand=1)
+				factor = (size/res.width)
+				resScaled = res.resize((int(res.width * factor), int(res.height * factor)))
+				alpha = resScaled.split()[-1]
+				bt = N.zeros((size,size, 4), dtype=N.float32)
+				bt[:,:,0] = 1
+				bt[:,:,1] = 1
+				bt[:,:,2] = 1		
+				bt[:,:,3] = N.divide(N.array(alpha), 255)
+				self.brushTip.append(bt)
+			self.multibrush = True
+		else:
+			#NormalBrush
+			res = Image.open(tipImage)
+			res = res.rotate(angle, expand=1)
+			factor = (size/res.width)
+			resScaled = res.resize((int(res.width * factor), int(res.height * factor)))
+			alpha = resScaled.split()[-1]
+			bt = N.zeros((size,size, 4), dtype=N.float32)
+			bt[:,:,0] = 1
+			bt[:,:,1] = 1
+			bt[:,:,2] = 1		
+			bt[:,:,3] = N.divide(N.array(alpha), 255)
+			self.brushTip = bt
+
 		
-		bt = N.zeros((size,size, 4), dtype=N.float32)
-		bt[:,:,0] = 1
-		bt[:,:,1] = 1
-		bt[:,:,2] = 1		
-		bt[:,:,3] = N.divide(N.array(alpha), 255)
-		self.brushTip = bt
-		
+		#Set the perameters
 		self.brushSize = size
 
 
 		#Set the brush mask
-		#self.maskSize = 512
-		#res = Image.open("paperGrain.png")
-		#factor = (self.maskSize/res.width)
-		#resScaled = res.resize((int(res.width * factor), int(res.height * factor)))
-		#alpha = resScaled.split()[0] #Take only the red channel since is black and white
-		#bm = N.zeros((self.maskSize,self.maskSize), dtype=N.float32)
-		#bm[:,:] = N.divide(N.array(alpha), 255)
-		#self.brushMask = bm
+		self.maskSize = 2048
+		res = Image.open("paperGrain2048.png")
+		factor = (self.maskSize/res.width)
+		resScaled = res.resize((int(res.width * factor), int(res.height * factor)))
+		alpha = resScaled.split()[0] #Take only the red channel since is black and white
+		bm = N.zeros((self.maskSize,self.maskSize), dtype=N.float32)
+		bm[:,:] = N.divide(N.array(alpha), 255)*5
+		self.brushMask = 1-bm
 
 
 
 	def makeDab(self, layer, x, y, color):
 		#Get the slice and uniform to [0-1]
-		destination = layer[y:y+self.brushSize,x:x+self.brushSize].astype(N.float32)
+		destination = layer[y:y+self.brushSize, x:x+self.brushSize].astype(N.float32)
 		destination /= 255
 
-		#(50,50,4)  (50,50,4)     (4)
-		source = N.zeros((50,50, 4), dtype=N.float32)
-		source[:,:,0] = self.brushTip[:,:,0] * color[0]
-		source[:,:,1] = self.brushTip[:,:,1] * color[1]
-		source[:,:,2] = self.brushTip[:,:,2] * color[2]
-		source[:,:,3] = self.brushTip[:,:,3] * color[3]
-
-		#Use the SRC alpha ADD DEST inverse_alpha blending method
-		final = N.zeros((50,50, 4), dtype=N.float32)
+		#Calculate Source image 
+		brushSource = 0
+		if self.multibrush:
+			brushSource = self.brushTip[rand(0,len(self.brushTip)-1)]
+		else:
+			brushSource = self.brushTip
+		source = brushSource[:,:] * color
+		source[:,:,3] *= self.brushMask[y:y+self.brushSize, x:x+self.brushSize]
+		
+		#Apply source image over destination using the SRC alpha ADD DEST inverse_alpha blending method
+		final = N.zeros((self.brushSize,self.brushSize, 4), dtype=N.float32)
 		final[:,:,0] = ((destination[:,:,0] * (1-source[:,:,3])) + (source[:,:,0] * (source[:,:,3])))*255
 		final[:,:,1] = ((destination[:,:,1] * (1-source[:,:,3])) + (source[:,:,1] * (source[:,:,3])))*255
 		final[:,:,2] = ((destination[:,:,2] * (1-source[:,:,3])) + (source[:,:,2] * (source[:,:,3])))*255		
 		final[:,:,3] = ((destination[:,:,3] * (1-source[:,:,3])) + (source[:,:,3] * (source[:,:,3])))*255
-		final = final.astype(N.uint8)
-		layer[y:y+self.brushSize,x:x+self.brushSize] = final
-
-
-
-'''
-brush 		= [1, 1, 1, 0.5]
-color 		= [1, 1, 1, 1]
-
-source 		= [1, 1, 1, 0.5]
-
-
-1*0.5 + 0.5*0.5
-0.5 + 0.25
-0.75
-
-
-destination = [1, 1, 1, 1]
-
-
-actual 		= []
-
-'''
-
-
+		layer[y:y+self.brushSize,x:x+self.brushSize] = final.astype(N.uint8)
 
 
 
--- paynt.py
+++ paynt.py
@@ -9,18 +9,24 @@ import time
 start = time.time()
 paynter = Paynter()
 
+pencil = Brush("gradient_2.png","paperGrain.png",size=50,angle=45)
+watercolor = Brush(["watercolor1_2.png","watercolor2_2.png","watercolor3_2.png","watercolor4_2.png","watercolor5_2.png"],"",size=220,angle=0)
 
-data = N.zeros((512,512, 4), dtype=N.uint8)
+data = N.zeros((2048, 2048, 4), dtype=N.uint8)
 data[:,:,3] = 255
 data[:,:256,:3] = 255
 
+
+
+
 paynter.setLayer(data)
-paynter.setBrush(Brush("gradient.png","paperGrain.png"))
+paynter.setBrush(pencil)
 paynter.setColor(0, 0, 0, 255)
 paynter.drawLine(100, 100, 300, 100)
 paynter.setColor(255, 255, 255, 255)
 paynter.drawLine(100, 200, 300, 200)
 
+paynter.setBrush(watercolor)
 paynter.setColor(255, 0, 0, 255)
 paynter.drawLine(100, 350, 300, 350)
 paynter.setColor(0, 255, 0, 255)
@@ -32,6 +38,10 @@ data[:,:,3] = 255
 
 end = time.time()
 print('Execution time: '+str(end-start))
+
+
+
+
 
 
 img = Image.fromarray(data, 'RGBA')
