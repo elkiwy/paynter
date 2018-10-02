061f950608201d66278d1999c3e1aa4380a7b02a
Stefano Bertoli <elkiwydev@gmail.com>
2018-10-02 20:51:10 +0200

    Added spacing Brush parameter, changed drawline function to use proper spacing

--- functions.py
+++ functions.py
@@ -2,10 +2,38 @@ import numpy as N
 from PIL import Image
 import PIL.ImageOps
 import random
+import math
 
+'''
+TODO:
+-Do a proper wrapping around brush textures
+-Add spacing to brushes
+-Add randomization parameters for waterolours
+-reimplement rotation on the dab step
+-Check for dab outside the layer
+
+
+'''
+
+
+
+######################################################################
+# Useful Functions
+######################################################################
+
+#Random shortcut
 def rand(a,b):
 	return random.randint(a,b) #inclusive
 
+#Degree cosine shortcut
+def dcos(deg):
+	return math.cos(math.radians(deg))
+
+#Degree sine shortcut
+def dsin(deg):
+	return math.sin(math.radians(deg))
+
+#Image to brushtip function
 def loadBrushTip(path, size):
 	#Open the image and make sure its RGB
 	res = Image.open(path).convert('RGB')
@@ -27,56 +55,62 @@ def loadBrushTip(path, size):
 	return bt
 
 
-'''
-TODO:
--Do a proper wrapping around brush textures
--Add spacing to brushes
--Add randomization parameters for waterolours
--reimplement rotation on the dab step
-
-
-
-'''
-
-
-
-
-
-
-
-
+######################################################################
+# Main classes
+######################################################################
+#The paynter class is the object that draw stuff on a layer using a brush and a color
 class Paynter:
 	brush = 0
 	layer = 0
 	color = [0,0,0,0]
 
 	def drawLine(self, x1, y1, x2, y2):
-		for i in N.arange(0, 1, 1):
-			x = x1 + i*(x2-x1)
-			y = y1 + i*(y2-y1)
-			#self.layer[int(x), int(y)] = self.color
+		#Calculate the direction and the length of the step
+		direction = math.atan2(y2 - y1, x2 - x1)
+		print('direction:'+str(direction))
+		length = self.brush.spacing
+
+		#Prepare the loop
+		x, y = x1, y1
+		currentDist = math.sqrt((x2 - x)**2 + (y2 - y)**2)
+		previousDist = currentDist+1
+		
+		#Do all the steps until I passed the target point
+		while(previousDist>currentDist):
+			#Make the dab on this point
 			self.brush.makeDab(self.layer, int(x), int(y), self.color)
 
-	def setColor(self, r, g, b, a):
+			#Mode the point for the next step and update the distances
+			x += length*dcos(direction)
+			y += length*dsin(direction)
+			previousDist = currentDist
+			currentDist = math.sqrt((x2 - x)**2 + (y2 - y)**2)
+			print(currentDist)
+			
+	#Setter for color, takes 0-255 RGBA
+	def setColor(self, r, g, b, a=255):
 		self.color = N.divide([r, g, b, a], 255)
 
+	#Setter for layer reference
 	def setLayer(self, array):
 		self.layer = array
 
+	#Setter for brush reference
 	def setBrush(self, b):
 		self.brush = b
 
 
-
+#The brush class is the one that define how the current brush should behave
 class Brush:
 	brushTip = 0
 	brushMask = 0
-	#fuzzySize = [0.45, 0.55]
 	brushSize = 0
 	maskSize = 0
 	multibrush = False
+	spacing = 0
 
-	def __init__(self, tipImage, maskImage, size=50, color=[0,0,0,0],angle=0):
+	def __init__(self, tipImage, maskImage, size = 50, 
+				color = [0,0,0,0], angle = 0, spacing = 1):
 		#Set the brushTip
 		if isinstance(tipImage, list):
 			#Multibrush
@@ -88,11 +122,10 @@ class Brush:
 		else:
 			#NormalBrush
 			self.brushTip = loadBrushTip(tipImage, size)
-
 		
 		#Set the perameters
 		self.brushSize = size
-
+		self.spacing = size*spacing
 
 		#Set the brush mask
 		self.maskSize = 2048
--- paynt.py
+++ paynt.py
@@ -9,9 +9,19 @@ import time
 start = time.time()
 paynter = Paynter()
 
-pencil = Brush("gradient.png","paperGrain2048.png",size=50,angle=45)
-#watercolor = Brush(["watercolor1_2.png","watercolor2_2.png","watercolor3_2.png","watercolor4_2.png","watercolor5_2.png"],"",size=220,angle=0)
-watercolor = Brush(["watercolor1.png","watercolor2.png","watercolor3.png","watercolor4.png","watercolor5.png"],"",size=220,angle=0)
+pencil = Brush(
+	"gradient.png",
+	"paperGrain2048.png",
+	size=50,
+	angle=45,
+	spacing=0.05)
+
+watercolor = Brush(
+	["watercolor1.png","watercolor2.png","watercolor3.png","watercolor4.png","watercolor5.png"],
+	"",
+	size=220,
+	angle=0,
+	spacing = 0.75)
 
 data = N.zeros((2048, 2048, 4), dtype=N.uint8)
 data[:,:,3] = 255
@@ -23,19 +33,19 @@ data[:,:256,:3] = 255
 paynter.setLayer(data)
 paynter.setBrush(pencil)
 paynter.setColor(0, 0, 0, 255)
-paynter.drawLine(100, 100, 300, 100)
+paynter.drawLine(100, 100, 1800, 100)
 paynter.setColor(255, 255, 255, 255)
-paynter.drawLine(100, 200, 300, 200)
+paynter.drawLine(100, 200, 1800, 200)
 
 paynter.setBrush(watercolor)
 paynter.setColor(255, 0, 0, 255)
-paynter.drawLine(100, 350, 300, 350)
+paynter.drawLine(100, 350, 1000, 350)
 paynter.setColor(0, 255, 0, 255)
-paynter.drawLine(100, 400, 300, 400)
+paynter.drawLine(100, 400, 1000, 400)
 paynter.setColor(0, 0, 255, 255)
-paynter.drawLine(100, 450, 300, 450)
+paynter.drawLine(100, 450, 1000, 550)
 
-paynter.drawLine(500, 450, 500, 450)
+#paynter.drawLine(500, 450, 500, 450)
 
 data[:,:,3] = 255
 
